fe771400f15eba861f3ecb2041a643e50cdb0a6b
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2021-11-17 22:15:12 -0500

    form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,13 @@ var taskFormHandler = function(event) {
     //the .value (see notes below)
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    //check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
+    
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
