179932f7214734d682697a453534e86527b878cd
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2021-11-17 21:34:03 -0500

    form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,29 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value; //the input is from the html, use the [] for selecting "name" of input from html.
+    //the .value (see notes below)
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+    //create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className - "task-item";
+
+    //create div to hold task info and add list item
+    var taskInfoEl = document.createElement("div");
+    //give it a class name
+    taskInfoEl.className = "task-info";
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task"
+    listItemEl.textContent = taskNameInput;
     tasksToDoEl.appendChild(listItemEl);
 };
 
@@ -19,3 +38,5 @@ formEl.addEventListener("submit", createTaskHandler);
 //     listItemEl.textContent = "This is a new task.";
 //     tasksToDoEl.appendChild(listItemEl);
 // });
+
+// console.dir(taskNameInput); //the console.dir lets us see more info re what users input (value)
